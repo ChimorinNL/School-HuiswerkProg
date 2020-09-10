3f7a97bb7d538a0d1cf0c9022e1d8485bb15faaf
Tom <33694551+ChimorinNL@users.noreply.github.com>
2020-09-10 10:27:43 +0200

    Changes

---
+++ Week2/Week2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 2 ==0)
+                    Console.WriteLine("Hello World!");
+            }
+            Console.ReadKey();
+
+            int teller = 0;
+            while (teller < 10)
+            {
+                Console.WriteLine("Dobby" + teller);
+                teller++;
+
+            }
+            Console.ReadKey();
+
+            bool asleep = true;
+            while (asleep)
+            {
+                Console.WriteLine("Ben je al wakker?");
+                asleep = Console.ReadLine() != "ja";
+            }
+            Console.WriteLine("Mooi dat je wakker bent!");
+            Console.ReadKey();
+        }
+    }
+}
